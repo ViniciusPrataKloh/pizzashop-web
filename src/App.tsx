176b17c65286c989c45035f534df3c056811c9ad
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2024-01-03 19:44:07 -0300

    Config Helmet to set title template

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './global.css'
 
+import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { BrowserRouter } from 'react-router-dom'
 
 import { Router } from './Router'
@@ -7,9 +8,13 @@ import { Router } from './Router'
 function App() {
   return (
     <div className="flex h-screen items-center justify-center bg-gray-500 ">
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <HelmetProvider>
+        <Helmet titleTemplate="%s | pizza-shop" />
+
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </HelmetProvider>
     </div>
   )
 }
