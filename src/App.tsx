066bd2dfecd0864350f7500fae81782c86395b12
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2024-01-03 12:58:37 -0300

    Base project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from './components/ui/button';
+import './global.css';
+
+
+function App() {
+  return (
+    <div className='flex h-screen items-center justify-center bg-gray-500'>
+      <Button>Enviar</Button>
+    </div>
+  )
+}
+
+export default App
