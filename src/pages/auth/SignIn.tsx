5f19079766952331706c593f4adec6b1929c1f13
ViniciusPrataKloh <vinicius.prata.kloh@gmail.com>
2024-01-12 15:37:11 -0300

    SignIn page

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -1,31 +1,65 @@
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import { Link } from 'react-router-dom'
+import { toast } from 'sonner'
+import * as z from 'zod'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
+const signInFormSchema = z.object({
+  email: z.string().email(),
+})
+
+type SignInForm = z.infer<typeof signInFormSchema>
+
 export default function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>()
+
+  async function handleSignIn(data: SignInForm) {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    console.log(data)
+    toast.success('Enviamos um link de autenticação para o seu e-mail.', {
+      action: {
+        label: 'Reenviar',
+        onClick: () => handleSignIn(data),
+      },
+    })
+    reset()
+  }
+
   return (
     <section className="flex items-center justify-center">
       <Helmet title="Sign-In" />
 
       <div className="p-8">
+        <Button className="absolute right-8 top-8" asChild>
+          <Link to="/auth/sign-up">Novo cadastro</Link>
+        </Button>
+
         <div className="flex w-[350px] flex-col justify-center gap-6">
           <div className="flex flex-col gap-2 text-center">
             <h1 className="text-2xl font-semibold tracking-tight">
               Acessar painel
             </h1>
-            <p className="text-muted-foreground text-sm">
+            <p className="text-sm text-muted-foreground">
               Acompanhe suas vendas pelo painel do parceiro!
             </p>
           </div>
 
-          <form className="space-y-4">
+          <form className="space-y-4" onSubmit={handleSubmit(handleSignIn)}>
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
-              <Input id="email" type="email" />
+              <Input id="email" type="email" {...register('email')} />
             </div>
-            <Button className="w-full" type="submit">
+            <Button className="w-full" type="submit" disabled={isSubmitting}>
               Acessar painel
             </Button>
           </form>
--- src/pages/layouts/AuthLayout.tsx
+++ src/pages/layouts/AuthLayout.tsx
@@ -4,8 +4,8 @@ import { Outlet } from 'react-router-dom'
 export function AuthLayout() {
   return (
     <div className="grid min-h-screen grid-cols-2">
-      <div className="border-foreground/5 bg-muted text-muted-foreground flex flex-col justify-between border-r p-10">
-        <div className="text-foreground flex items-center gap-3 text-lg">
+      <div className="flex flex-col justify-between border-r border-foreground/5 bg-muted p-10 text-muted-foreground">
+        <div className="flex items-center gap-3 text-lg text-foreground">
           <Pizza className="h-5 w-5" />
           pizza.shop
         </div>
@@ -15,7 +15,9 @@ export function AuthLayout() {
         </footer>
       </div>
 
-      <Outlet />
+      <div className="relative flex flex-col items-center justify-center">
+        <Outlet />
+      </div>
     </div>
   )
 }
